17ad4a551618204289abc2aac5f028cae9dce48b
greybeard <greybeard@gmail.com>
2024-06-09 08:14:57 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.engine.file_storage import classes
+from models import storage
 
 
 @app_views.route('/status', methods=["GET"], strict_slashes=False)
 def status():
     """returns a JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=["GET"], strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    new_dict = {}
+    for cls in classes:
+        if cls != "BaseModel":
+            new_dict[cls] = storage.count(cls)
+
+    return jsonify(new_dict)
