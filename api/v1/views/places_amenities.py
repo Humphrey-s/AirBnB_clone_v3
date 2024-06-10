cf49cd866e360e65c1437dec99989330b0eb8e92
greybeard <greybeard@gmail.com>
2024-06-10 07:19:07 +0300

    db amenity | file amenity

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import make_response, jsonify, abort, request
 from models import storage
+from os import environ
 from models.user import User
 from models.state import State
 from models.place import Place
@@ -19,6 +20,7 @@ classes = {
             "Review": Review
             }
 
+storage_t = environ.get('HBNB_TYPE_STORAGE')
 
 @app_views.route(
         '/places/<place_id>/amenities',
@@ -28,32 +30,15 @@ classes = {
 def get_place_amenities(place_id):
     """get amenities of a place"""
     place = storage.get(Place, place_id)
-    amenities = storage.all(Amenity)
 
     if place is None:
         abort(404)
 
     place_amenity = []
-    for amenity in amenities.values():
-        if amenity.place_id == place_id:
-            place_amenity.append(review.to_dict())
+    for amenity in place.amenities():
+            place_amenity.append(amenity.to_dict())
 
     return make_response(jsonify(place_amenity), 200)
-
-
-@app_views.route(
-        '/amenities/<amenity_id>',
-        methods=["GET"],
-        strict_slashes=False)
-def get_place_amenity(amenity_id):
-    """get a specific amenity of a place"""
-    amenity = storage.get(Amenity, amenity_id)
-
-    if amenity is None:
-        abort(404)
-
-    return make_response(jsonify(amenity.to_dict()), 200)
-
 
 @app_views.route(
         '/places/<place_id>/amenities/<amenity_id>',
@@ -71,7 +56,13 @@ def delete_place_amenity(place_id, amenity_id):
     if amenity is None:
         abort(404)
 
-    storage.delete(amenity)
+    if storage_t == "db":
+        if amenity in place.amenities:
+            place.amenities.remove(amenity)
+    else:
+        if amenity.id in place.amenity_ids:
+            place.amenity_ids.remove(amenity.id)
+
     storage.save()
     return jsonify({}), 200
 
@@ -81,7 +72,7 @@ def delete_place_amenity(place_id, amenity_id):
         methods=["POST"],
         strict_slashes=False)
 def create_place_amenity(place_id):
-    """create a place review"""
+    """create a place amenity"""
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
@@ -91,17 +82,17 @@ def create_place_amenity(place_id):
     if amenity is None:
         abort(404)
 
-    if not request.get_json(force=True):
-        abort(400, description="Not a JSON")
+    if storage_t == "db":
+        if amenity not in place.amenities:
+            place.amenities.append(amenity)
+        else:
+            return make_response(jsonify(amenity.to_dict())), 200
+    else:
+        if amenity.id not in place.amenity_ids:
+            place.amenity_ids.append(amenity.id)
+        else:
+            return make_response(jsonify(amenity.to_dict())), 200
 
-    data = request.get_json()
-    data["place_id"] = place.id
-    ignore = ["id", "created_at", "updated_at"]
 
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(amenity, key, value)
-
-    amenity.save()
     storage.save()
     return jsonify(amenity.to_dict()), 201
